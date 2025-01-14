b532139017358af174a1d7e342b25ea9c8db3f28
himalf <himal.fullel15@gmail.com>
2025-01-14 16:59:37 +0545

    feat: setup shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
+import Homepage from "./pages/homepage";
 function App() {
   return (
     <main className="mx-[10%]">
       <Router>
-        <MainLayout children />
-        <Routes>
-          <Route />
-        </Routes>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+          </Routes>
+        </MainLayout>
       </Router>
     </main>
   );
