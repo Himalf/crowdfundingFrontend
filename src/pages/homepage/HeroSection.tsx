32111cb00e11a5904c0575c2346aae6d48610193
himalf <himal.fullel15@gmail.com>
2025-01-14 18:28:02 +0545

    feat: add images in carousel content

--- src/pages/homepage/HeroSection.tsx
+++ src/pages/homepage/HeroSection.tsx
@@ -6,11 +6,31 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  //   CarouselNext,
-  //   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Donation1 from "@/assets/donate3.png";
+import Donation2 from "@/assets/donate4.png";
+import Donation3 from "@/assets/donate5.png";
+import Donation4 from "@/assets/donate6.png";
+import Donation5 from "@/assets/donate7.png";
 
 const HeroSection = () => {
+  const heroImage = [
+    {
+      name: Donation1,
+    },
+    {
+      name: Donation2,
+    },
+    {
+      name: Donation3,
+    },
+    {
+      name: Donation4,
+    },
+    {
+      name: Donation5,
+    },
+  ];
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: false })
   );
@@ -20,12 +40,16 @@ const HeroSection = () => {
       className="w-full max-w-[70%] items-center mx-auto"
     >
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
+        {heroImage.map((val, i) => (
+          <CarouselItem key={i}>
             <div className="p-1">
               <Card>
-                <CardContent className="flex aspect-video items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+                <CardContent className="flex aspect-video items-center justify-center p-9">
+                  <img
+                    src={val.name}
+                    alt="hero images"
+                    className="h-full w-full object-contain"
+                  />
                 </CardContent>
               </Card>
             </div>
