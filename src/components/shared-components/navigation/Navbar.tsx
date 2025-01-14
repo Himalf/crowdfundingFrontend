14d4b7c004e03cc8f259870e02be14a85e1bba20
himalf <himal.fullel15@gmail.com>
2025-01-14 14:40:38 +0545

    style: use primary and secondary colors and use flex for design

---
+++ src/components/shared-components/navigation/Navbar.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import { BiDonateHeart } from "react-icons/bi";
+const Navbar = () => {
+  const navItems = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Donation",
+      path: "/donation",
+    },
+    {
+      name: "How it works",
+      path: "/how-it-works",
+    },
+    {
+      name: "About us",
+      path: "/aboutus",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center mt-4 bg-primaryColor py-5 px-2 rounded-t-lg">
+      <div className="flex gap-2 items-center">
+        <h1 className="text-3xl font-bold cursor-pointer flex items-center gap-1 ">
+          <span className="text-secondaryColor">
+            <BiDonateHeart />
+          </span>
+          <span>Fund</span>
+        </h1>
+        <span className="border border-r-2 border-secondaryColor h-8"></span>
+        <ul className="flex items-center gap-6">
+          {navItems.map((val, i) => (
+            <Link to={val.path} key={i}>
+              <li>{val.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="bg-secondaryColor text-white font-sm font-semibold px-4 py-1 rounded-md">
+        <button>Download app</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
